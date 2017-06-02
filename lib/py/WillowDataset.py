b6a2bab6486f65c173f6dd2e0e2f241fc40b639c
Charlie Lamantia <cblam@leaflabs.com>
2017-06-02 13:32:22 -0400

    # # Changes: # #     lib/py/WillowDataset.py
    Summary: upgrade to new implementation, stolen from willow-gui 2.0.0
    
    Test Plan: Run old stuff and see what borked
    
    Reviewers: #willow, chrono
    
    Reviewed By: #willow, chrono
    
    Subscribers: chrono, #willow
    
    Differential Revision: https://reviews.leaflabs.com/D122

--- lib/py/WillowDataset.py
+++ lib/py/WillowDataset.py
@@ -1,46 +1,56 @@
 import numpy as np
+from scipy import signal as dsp
+import multiprocessing as mp
 import h5py
 from PyQt4 import QtCore
-import scipy.signal as signal
 
-IMPORT_LIMIT_GB = 8
-MAX_NSAMPLES = IMPORT_LIMIT_GB*5e5
+
+# general parameters
+NCHAN = 1024
+NCHIPS = 32
+SAMPLE_RATE = 3e4
+MS_PER_SEC = 1000
 MICROVOLTS_PER_COUNT = 0.195
 
-def analyzeBuffer(buff):
-    buffarray = np.array(buff)
-    maxind = np.argmax(buffarray[:,1])
-    return buff[maxind][0]
+# filter parameters
+FILT_LOWCUT_HZ = 100.
+FILT_HIGHCUT_HZ = 9500.
+FILT_ORDER = 5
+NYQUIST_RATE = SAMPLE_RATE / 2.
+FILTER_B, FILTER_A = dsp.butter(FILT_ORDER,
+                        [FILT_LOWCUT_HZ/NYQUIST_RATE, FILT_HIGHCUT_HZ/NYQUIST_RATE],
+                        btype='band')
 
-def threshold(indata, thresh=None):
-    if thresh==None:
-        print 'Threshold not defined, here is min mean and max:'
-        print np.min(indata), np.mean(indata), np.max(indata)
-    else:
-        # next two lines make the algorithm agnostic of polarity (pos/neg)
-        polarity = -1. if (thresh < 0) else 1.
-        thresh = abs(thresh)
-        # init buffers and counters
-        recording = False
-        buff = []
-        stats = []
-        nspikes = 0
-        # run over the data
-        for i,samp in enumerate(polarity*indata):
-            if not recording:
-                if samp>=thresh:
-                    buff.append((i,samp))
-                    recording = True
-            else:
-                if samp<thresh:
-                    recording = False
-                    stats.append(analyzeBuffer(buff))
-                    nspikes += 1
-                    buff = []
-                else:
-                    buff.append((i,samp))
+# activity calculation parameters
+THRESH_SCALE = -4.5/0.6745 # from JP Kinney
+ACTIVITY_SCALE = 100. # arbitrary scaling to get activity approximately normalized to [0,1]
 
-        return stats, nspikes
+def filterAndCalculateActivity(data, wpipe):
+    # works on uv data
+    nchan, nsamp = data.shape
+    filteredData = dsp.lfilter(FILTER_B, FILTER_A, data, axis=1)
+    wpipe.send(filteredData)
+    threshold = np.median(np.abs(filteredData), axis=1)*THRESH_SCALE
+    activity = np.zeros(nchan)
+    for i in range(nchan):
+        activity[i] = np.sum((filteredData[i,:] < threshold[i])) * ACTIVITY_SCALE / float(nsamp)
+    wpipe.send(activity)
+
+
+# the following two functions facilitate multiprocessing for the activity calc
+
+def MPGenerator(dataSlice, nproc):
+    nchan = dataSlice.shape[0]
+    nchanPerProc = nchan // nproc
+    splitter = np.arange(1,nproc) * nchanPerProc
+    for subSlice in np.split(dataSlice, splitter):
+        yield subSlice
+
+class PipedProcess(mp.Process):
+    def __init__(self, rpipe, **kwargs):
+        mp.Process.__init__(self, **kwargs)
+        self.rpipe = rpipe
+
 
 class WillowImportError(Exception):
     pass
@@ -51,156 +61,78 @@ class WillowDataset(QtCore.QObject):
     Common format for passing data between import processes, plot windows, etc.
     """
 
-    progressUpdated = QtCore.pyqtSignal(int)
-
-    def __init__(self, filename, sampleRange):
+    def __init__(self, filename):
         QtCore.QObject.__init__(self)
+
         self.filename = filename
         self.fileObject = h5py.File(self.filename)
-        # the following test allows for backward-compatibility
-        if 'wired-dataset' in self.fileObject:
-            self.isOldLayout = True
-            self.dset = self.fileObject['wired-dataset']
-            # determine type based on header flag
-            if (self.dset[0][0] & (1<<6)):
-                self.type = 'snapshot'
-            else:
-                self.type = 'experiment'
-        else:
-            self.isOldLayout = False 
-            self.dset = self.fileObject['channel_data']
-            # determine type based on header flag
-            if self.fileObject['ph_flags'][0] & (1<<6):
-                self.type = 'snapshot'
-            else:
-                self.type = 'experiment'
-
-        # define self.sampleRange and related temporal data
-        if sampleRange==-1:
-            if self.isOldLayout:
-                self.nsamples = len(self.dset)
-            else:
-                self.nsamples = len(self.dset)//1024
-            self.sampleRange = [0, self.nsamples-1]
-        else:
-            self.sampleRange = sampleRange
-            self.nsamples = self.sampleRange[1] - self.sampleRange[0] + 1
-            if self.isOldLayout:
-                dsetMin = int(self.dset[0][1])
-                dsetMax = int(self.dset[-1][1])
-            else:
-                dsetMin = int(self.fileObject['sample_index'][0])
-                dsetMax = int(self.fileObject['sample_index'][-1])
-            if self.type=='snapshot':
-                # need to normalize because snapshots have random offsets
-                dsetMax -= dsetMin
-                dsetMin = 0
-            if (self.sampleRange[0] < dsetMin) or (self.sampleRange[1] > dsetMax):
-                raise IndexError('Error: sampleRange [%d, %d] out of range for dset: [%d, %d]'
-                    % tuple(self.sampleRange+[dsetMin,dsetMax]))
-        self.time_ms = np.arange(self.sampleRange[0], self.sampleRange[1]+1)/30.
+        self.dset = self.fileObject['channel_data']
+        self.isSnapshot = self.fileObject.attrs['ph_flags'][0] & (1 << 6)
+        self.nsamples, self.nchan = self.dset.shape
+        self.time_ms = np.arange(self.nsamples)*MS_PER_SEC/SAMPLE_RATE
         self.timeMin = np.min(self.time_ms)
         self.timeMax = np.max(self.time_ms)
-
-        # other metadata
-        if self.isOldLayout:
-            self.boardID = self.dset.attrs['board_id'][0]
-            if self.type=='experiment':
-                self.cookie = self.dset.attrs['experiment_cookie'][0]
-            else:
-                self.cookie = None
-            chipAliveMask = self.dset[0][2]
+        self.boardID = self.fileObject.attrs['board_id'][0]
+        if self.isSnapshot:
+            self.cookie = None
         else:
-            self.boardID = self.fileObject.attrs['board_id'][0]
-            if self.type=='experiment':
-                self.cookie = self.fileObject.attrs['experiment_cookie'][0]
-            else:
-                self.cookie = None
-            chipAliveMask = self.fileObject['chip_live'][0]
-        self.chipList = [i for i in range(32) if (chipAliveMask & (0x1 << i))]
+            self.cookie = self.fileObject.attrs['experiment_cookie'][0]
+        chipAliveMask = self.fileObject['chip_live'][0]
+        self.chipList = [i for i in range(NCHIPS) if (chipAliveMask & (0x1 << i))]
 
-        # flags
         self.isImported = False
-        self.isFiltered = [False]*1024
-        self.data_uv_filtered = np.zeros((1024,self.nsamples), dtype='float')
 
-        # spikedetection
-        self.spikeThresholds = {}
-        self.spikeIndices = {}
-        self.nspikes = {}
-        self.spikeTimes= {}
+        self.ncpu = mp.cpu_count()
 
     def importData(self):
-        if self.isOldLayout:
-            if self.nsamples > MAX_NSAMPLES:
-                raise WillowImportError
-            self.data_raw = np.zeros((1024,self.nsamples), dtype='uint16')
-            for i in range(self.nsamples):
-                self.data_raw[:,i] = self.dset[self.sampleRange[0]+i][3][:1024]
-                if (i%1000==0):
-                    self.progressUpdated.emit(i)
-        else:
-            self.progressUpdated.emit(0)
-            self.data_raw = np.array(self.fileObject['channel_data'][self.sampleRange[0]
-                *1024:(self.sampleRange[1]+1)*1024], dtype='uint16').reshape(
-                (self.nsamples, 1024)).transpose()
-            self.data_aux = np.array(self.fileObject['aux_data'][self.sampleRange[0]
-                *96:(self.sampleRange[1]+1)*96], dtype='uint16').reshape(
-                (self.nsamples, 96)).transpose()
-            self.sample_index = np.array(self.fileObject['sample_index'][self.sampleRange[0]:
-                                self.sampleRange[1]+1])
-            self.parseGPIO()
-        self.progressUpdated.emit(self.nsamples)
+        self.data_raw = self.dset[:].transpose()
         self.data_uv = (np.array(self.data_raw, dtype='float')-2**15)*MICROVOLTS_PER_COUNT
         self.dataMin = np.min(self.data_uv)
         self.dataMax = np.max(self.data_uv)
         self.limits = [self.timeMin, self.timeMax, self.dataMin, self.dataMax]
         self.isImported = True
 
-    def parseGPIO(self):
-        self.GPIOindices = np.where(self.sample_index % 15 == 11)[0]
-        self.GPIOtimes = self.time_ms[self.GPIOindices]
-        # GPIO gets copied into slot 1 for all 32 chips, so 1,4,7.. would all work
-        GPIO_bitfield = self.data_aux[1, self.GPIOindices]
-        self.GPIO = np.zeros((16,len(GPIO_bitfield)))
-        for i in range(16):
-            self.GPIO[i,:] = np.bool_(GPIO_bitfield & (1 << i))
+    def importSlice(self, s0, s1):
+        # s0, s1 are the first and last sample indices of the slice
+        self.slice_raw = self.dset[s0:s1,:].transpose()
+        # cast to float, center on zero (subtract 2**16/2 = 2**15), and convert to microvolts
+        self.slice_uv = (np.array(self.slice_raw, dtype='float')-2**15)*MICROVOLTS_PER_COUNT
+        self.slice_s0 = s0
+        self.slice_s1 = s1
+        self.slice_nsamples = s1 - s0
+        self.slice_min = np.min(self.slice_uv)
+        self.slice_max = np.max(self.slice_uv)
 
-    def filterData(self, channelList=range(1024)):
-        lowcut = 300.
-        highcut = 9500.
-        fs = 3e4
-        order = 5
-        b, a = signal.butter(order, [lowcut*2./fs, highcut*2./fs], btype='band')
-        for chan in channelList:
-            if not self.isFiltered[chan]:
-                self.data_uv_filtered[chan,:] = signal.filtfilt(b, a, self.data_uv[chan,:])
-                self.isFiltered[chan] = True
+    def filterSlice(self):
+        self.slice_filtered = dsp.lfilter(FILTER_B, FILTER_A, self.slice_uv, axis=1)
 
-    def detectSpikes(self, channelList=range(1024), thresh='auto'):
-        for i in channelList: 
-            tmpChannel = self.data_uv_filtered[i,:]
-            if thresh=='auto':
-                thresh = -4.5*np.median(np.abs(tmpChannel))/0.6745  # from Justin
-            self.spikeIndices[i], self.nspikes[i] = threshold(tmpChannel, thresh)
-            self.spikeTimes[i] = self.time_ms[self.spikeIndices[i]]
-            self.spikeThresholds[i] = thresh
+    def filterAndCalculateActivitySlice(self):
+        # (multi) processing
+        self.slice_activity = np.zeros(NCHAN)
+        self.slice_filtered = np.zeros((NCHAN, self.slice_nsamples))
+        procs = []
+        for subSlice in MPGenerator(self.slice_uv, self.ncpu):
+            rpipe, wpipe = mp.Pipe()
+            procs.append(PipedProcess(rpipe, target=filterAndCalculateActivity,
+                            args=(subSlice, wpipe)))
+        for pp in procs: pp.start()
+        cursor = 0
+        for i, pp in enumerate(procs):
+            filteredData_subslice = pp.rpipe.recv()
+            pp.join()
+            activity_subslice = pp.rpipe.recv()
+            nchan_subslice = activity_subslice.shape[0]
+            self.slice_activity[cursor:(cursor+nchan_subslice)] = activity_subslice
+            self.slice_filtered[cursor:(cursor+nchan_subslice),:] = filteredData_subslice
+            cursor += nchan_subslice
 
     def applyCalibration(self, calibrationFile):
         self.calibrationFile = calibrationFile
         self.impedance = np.load(str(self.calibrationFile))
-        self.data_cal = np.zeros((1024, self.nsamples), dtype='float')
+        self.data_cal = np.zeros((NCHAN, self.nsamples), dtype='float')
         for (i, imp) in enumerate(self.impedance):
             print i
             if imp>0:
                 self.data_cal[i,:] = self.data_uv[i,:]*imp
             # else just leave it at zero
 
-if __name__=='__main__':
-    filename = '/home/chrono/neuro/data/hasenstaub/20151121/exp8_clicks_snapshot_20151121-191941.h5'
-    dataset = WillowDataset(filename, [0,30000-1])
-    dataset.importData()
-    import matplotlib.pyplot as plt
-    for i in range(16):
-        plt.plot(dataset.GPIO[i])
-        plt.show()
